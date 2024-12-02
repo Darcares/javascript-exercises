980df833c952a3b2969638d21506a2c39b874779
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-12-02 15:27:38 +0000

    Finish Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    let reversedString = "";
+    const arrayOfString = originalString.split("");
 
+    for(let i = arrayOfString.length - 1; i >= 0; i--) {
+        reversedString += arrayOfString[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line

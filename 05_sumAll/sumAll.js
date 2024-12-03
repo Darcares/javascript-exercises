1a2fb8e9ad88d309bf73476d0882a686b6e6caca
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-12-03 17:54:21 +0000

    Pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,19 @@
 const sumAll = function(int1, int2) {
     let sum = 0;
-    max = Math.max(int1, int2);
-    min = Math.min(int1, int2);
-    for(let i = min; i <= max; i++) {
-        sum += i;
+    if(    int1 < 0 
+        || int2 < 0 
+        || !Number.isInteger(int1) 
+        || !Number.isInteger(int2)
+        || Number.isNaN(int1)
+        || Number.isNaN(int2) 
+      ) return "ERROR";    
+      
+    else {
+        max = Math.max(int1, int2);
+        min = Math.min(int1, int2);
+        for(let i = min; i <= max; i++) {
+            sum += i;
+        }
     }
     return sum;
 };

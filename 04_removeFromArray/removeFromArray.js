d544f5fa7e9f36d372d7a00cc1d1fc498afce40d
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-12-03 13:51:02 +0000

    Finish Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,34 @@
-const removeFromArray = function(array, value) {
-    let currentValue;
-    let splicedArray = [];
-    for(let i = 0; i <= array.length; i++) {
-        currentValue = array[i];
-        if(currentValue === value) {
-            splicedArray = array.toSpliced(i, 1);
-        }            
+const removeFromArray = function(array, ...valuesToRemove) {
+    for(let i = 0; i <= valuesToRemove.length; i++) {
+        for(const valueToRemove of valuesToRemove) {
+            for(const value of array) {
+                if(value === valueToRemove) {
+                    const indexOfValueToRemove = array.indexOf(valueToRemove);
+                    array.splice(indexOfValueToRemove, 1);
+                }
+            }
+        }
     }
-    return splicedArray;
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// const removeFromArray = function(array, ...valuesToRemove) {
+//     for(let i = 0; i <= valuesToRemove.length; i++) {
+//         for(const value of array) {
+//             for(const valueToRemove of valuesToRemove) {
+//                 if(value === valueToRemove) {
+//                     const indexOfValueToRemove = array.indexOf(valueToRemove);
+//                     array.splice(indexOfValueToRemove, 1);
+//                 }
+//             }
+//         }
+//     }
+//     return array;
+// };
+
+//Both solutions work
+
+

3ab39e056f37a2449a4777ffb4ef3a7ff9265f68
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-12-03 09:39:25 +0000

    Passed 1st test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    let currentValue;
+    let splicedArray = [];
+    for(let i = 0; i <= array.length; i++) {
+        currentValue = array[i];
+        if(currentValue === value) {
+            splicedArray = array.toSpliced(i, 1);
+        }            
+    }
+    return splicedArray;
 };
 
 // Do not edit below this line

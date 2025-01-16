d8f9450b182d23ec3d25d5862be74135ecaa8178
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-01-16 18:50:11 +0000

    Finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people
+    .reduce((oldestPerson, nextPerson) => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        if(oldestPerson.yearOfDeath === undefined) oldestPerson.yearOfDeath = currentYear;
+        if(nextPerson.yearOfDeath === undefined) nextPerson.yearOfDeath = currentYear;
+        if(oldestPerson.yearOfDeath - oldestPerson.yearOfBirth < nextPerson.yearOfDeath - nextPerson.yearOfBirth) {
+            oldestPerson = nextPerson;
+        }
+        return oldestPerson;
+    });
 
 };
 

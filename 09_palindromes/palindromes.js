dcc0361edb135707db623c49aa53912ef7790be5
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-01-16 14:18:01 +0000

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (originalString) {
+    const originalArray = originalString.split(``);
+    const filteredArray = originalArray.filter((item) => {
+        if(item.charCodeAt(0) >= 32 && item.charCodeAt(0) <= 47) return false;
+        if(item.at(0) === ` `) return false;
+        return true;
+    });
+    const straightString = filteredArray.join(``).toLowerCase();
+    const reversedString = filteredArray.toReversed().join(``).toLowerCase();
+    if(straightString === reversedString) return true;
+    return false;
 };
 
 // Do not edit below this line

2414f21085c9a816554dc6ff7d22e1cdcf883368
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-01-16 15:19:36 +0000

    Change test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const intIndex = +index;
+    if(intIndex < 0) return `OOPS`;
+    let fibonacciSeries = [0, 1, 1];
+    for(let i = 2; i < intIndex; i++) {
+        const nextNumber = fibonacciSeries[i] + fibonacciSeries[i-1];
+        fibonacciSeries.push(nextNumber);
+    }
+    return fibonacciSeries[intIndex];
 };
 
 // Do not edit below this line

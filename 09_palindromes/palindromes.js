029b16b3938171cd4523564730d444233ae1f837
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-01-16 14:37:17 +0000

    Update

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,10 @@
 const palindromes = function (originalString) {
-    const originalArray = originalString.split(``);
-    const filteredArray = originalArray.filter(item => !((item.charCodeAt(0) >= 32 && item.charCodeAt(0) <= 47) || item.at(0) === ` `));
-    const straightString = filteredArray.join(``).toLowerCase();
-    const reversedString = filteredArray.toReversed().join(``).toLowerCase();
+    const straightString = originalString
+    .split(``)
+    .filter(item => !((item.charCodeAt(0) >= 32 && item.charCodeAt(0) <= 47) || item.at(0) === ` `))
+    .join(``)
+    .toLowerCase();
+    const reversedString = straightString.split(``).toReversed().join(``);
     if(straightString === reversedString) return true;
     return false;
 };

2c3a1ba981ffd884c4f38c8dbeb11315af5cc4d7
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-02 17:43:17 +0100

    Finish exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+
+    for (let index = 0; index < array.length; index++) {
+
+        elementsToRemove.forEach((elementToRemove) => {
+            if(array[index] === elementToRemove) {
+
+                array.splice(index, 1);
+                index--;
+            }
+        })
+        
+    }
+    
+    return array;
 };
 
 // Do not edit below this line

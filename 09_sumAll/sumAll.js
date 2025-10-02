2233d0842ff92aff7d278ef8938b860b95a1ee59
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-02 19:27:09 +0100

    Finish exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+
+    if(numberOne < 0 || numberTwo < 0) return "ERROR";
+    if(!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) return "ERROR";
+    if(Number.isNaN(numberOne) || Number.isNaN(numberTwo)) return "ERROR";
+
+    let smallNumber = 0;
+    let bigNumber = 0;
+    let sum = 0;
+    if(numberOne > numberTwo) {
+        bigNumber = numberOne;
+        smallNumber = numberTwo;
+    }
+
+    else {
+        bigNumber = numberTwo;
+        smallNumber = numberOne;
+    }
+
+    for(let index = smallNumber; index <= bigNumber; index++) {
+        sum += index;
+    }
+
+    return sum;
 
 };
 

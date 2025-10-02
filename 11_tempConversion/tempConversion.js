60554c7749d5d5796a15aaab5db55a3cce532ab6
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-02 23:25:15 +0100

    Finish exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  // °C = (°F - 32) × 5/9
+  const temperatureInCelsius = +((temperatureInFahrenheit - 32) *5/9).toFixed(1);
+
+  return temperatureInCelsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  // °F = (°C × 9/5) + 32
+  const temperatureInFahrenheit = +((temperatureInCelsius * 9/5) + 32).toFixed(1);
+
+  return temperatureInFahrenheit;
 };
 
 // Do not edit below this line

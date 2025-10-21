5bd875ff5fb4893acd32f1320db57bd514d83e66
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-21 02:39:05 +0100

    Finish exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let total = 0;
+
+  for (const number of numbers) {
+    total += number;
+  }
+
+	return total;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+    let total = 1;
+
+  for (const number of numbers) {
+    total *= number;
+  }
+
+	return total;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line

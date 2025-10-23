d2720916299b08b34d6e853bab163b0355ad8c06
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-23 04:26:50 +0100

    Finish exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(library) {
+    const titles = library.map(book => book.title);
+    return titles;
 };
 
 // Do not edit below this line

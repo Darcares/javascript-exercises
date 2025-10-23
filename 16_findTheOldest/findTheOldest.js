bc68025e6e0380ba6fadc0e2896e716ab201fe7c
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-23 04:43:23 +0100

    Finish exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((personOne, personTwo) => {
+        if (!("yearOfDeath" in personOne)) personOne.yearOfDeath = 2025;
+        if (!("yearOfDeath" in personTwo)) personTwo.yearOfDeath = 2025;
+        return (personTwo.yearOfDeath - personTwo.yearOfBirth) - (personOne.yearOfDeath - personOne.yearOfBirth);
+    });
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
